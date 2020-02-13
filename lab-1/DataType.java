0f74f0f43912c187848ba9fa7ec9ea82168100a8
SunilPdl <53456714+SunilPdl@users.noreply.github.com>
2020-02-13 20:26:18 +0545

    Add files via upload

---
+++ lab-1/DataType.java
@@ -0,0 +1,20 @@
+class DataType{
+    public static void main(String[] args)
+    {
+        int a = 10;
+        float b = 20.0f;
+        double c = 30.00;
+        long d = 40l;
+        short e = 50;
+        char f = 97;
+        boolean h = true;
+
+         System.out.println(a);
+         System.out.println(b);
+         System.out.println(c);
+         System.out.println(d);
+         System.out.println(e);
+         System.out.println(f);
+         System.out.println(h);
+    }
+}
